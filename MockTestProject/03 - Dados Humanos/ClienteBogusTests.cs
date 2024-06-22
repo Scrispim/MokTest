3cb8346eaee788acfd97630b86d2410c0f3ba610
Sander Crispim <sandercrispim@hotmail.com>
2024-06-22 12:47:02 -0300

    fix: sln

---
+++ MockTestProject/03 - Dados Humanos/ClienteBogusTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockTestProject._03___Dados_Humanos
+{
+    public class ClienteBogusTests
+    {
+    }
+}
