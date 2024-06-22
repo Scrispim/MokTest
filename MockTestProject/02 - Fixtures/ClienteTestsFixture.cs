c57da35affd2607dc6ca097f4fab204e34b3ff31
Sander Crispim <sandercrispim@hotmail.com>
2024-06-22 13:04:20 -0300

    feat: Bogus

--- MockTestProject/02 - Fixtures/ClienteTestsFixture.cs
+++ MockTestProject/02 - Fixtures/ClienteTestsFixture.cs
@@ -1,3 +1,5 @@
+using Bogus;
+using Bogus.DataSets;
 using Features.Clientes;
 
 namespace Features.Tests
@@ -10,15 +12,21 @@ namespace Features.Tests
     public class ClienteTestsFixture : IDisposable
     {
         public Cliente GerarClienteValido()
-        {            
-            var cliente = new Cliente(
-                Guid.NewGuid(),
-                "Eduardo",
-                "Pires",
-                DateTime.Now.AddYears(-30),
-                "edu@edu.com",
-                true,
-                DateTime.Now);
+        {
+            var genero = new Faker().PickRandom<Name.Gender>();
+
+            var cliente = new Faker<Cliente>("pt_BR")
+                .CustomInstantiator(f => new Cliente(
+                    Guid.NewGuid(),
+                    f.Name.FirstName(genero),
+                    f.Name.LastName(genero),
+                    f.Date.Past(80, DateTime.Now.AddYears(-18)),
+                    "",
+                    true,
+                    DateTime.Now))
+                .RuleFor(c => c.Email, (f, c) =>
+                f.Internet.Email(c.Nome.ToLower(), c.Sobrenome.ToLower()));
+             
 
                 return cliente;
         }
