8bc8cba12a604a0cdb0308e066b54fceb1fc0aaa
Sander Crispim <sandercrispim@hotmail.com>
2024-07-04 21:45:59 -0300

    push right code

--- MockTestProject/02 - Fixtures/ClienteTestsValido.cs
+++ MockTestProject/02 - Fixtures/ClienteTestsValido.cs
@@ -25,7 +25,7 @@ namespace Features.Tests
             var result = cliente.EhValido();
 
             // Assert 
-            Assert.False(result);
+            Assert.True(result);
             Assert.Equal(0, cliente.ValidationResult.Errors.Count);
         }
     }
